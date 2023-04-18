f18813c2c8e69beaf88a771187e39c483b97a9ee
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-18 18:29:45 -0500

    4/18/23
    step 28

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class Shape : MonoBehaviour
 {
     [Header("Rigidbody Object")]
-        public RigidBody2D rb;
+        public Rigidbody2D rb;
+
+        [Header("default shrinking sppeed")]
+        public float shrinkspeed = 3f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
+
+rb.rotation = Random.Range(0f, 360f);
+transform.localScale = Vector3.one * 10f;
+
+
+
+
+
+
+
 
     }
 
